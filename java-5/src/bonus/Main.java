83a23b038797ca9587669d5e5d4e647aa7108c1d
AlexandraOwl <nikolachuk98@mail.ru>
2021-04-21 22:21:02 +0300

    Новый файл

---
+++ java-5/src/bonus/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        long balance = 100;
+        long surcharge = 1100;
+        long bonus;
+        if (surcharge<1000){
+            bonus = surcharge / 100;
+        } else {
+            bonus =surcharge / 100;
+        } long totalBalance = balance + surcharge + bonus;
+            System.out.println(bonus);
+            System.out.println(totalBalance);
+        }
+    }
